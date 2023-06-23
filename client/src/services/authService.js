1631221f71abeae5ebd305fc5866f46ed057357d
Ganesh Patidar <ganesh9dyv@gmail.com>
2023-06-23 20:28:43 +0530

    Included Public and Protected Routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,12 @@
+import { userLogin, userRegister } from '../redux/features/auth/authActions';
+import store from '../redux/store'
+
 export const handleLogin=(e,email,password,role)=>{
     e.preventDefault();
     try{
-        console.log("Login",e,email,password,role);
+        if(!role || !email || !password)  return alert("All fields are required");
+
+        store.dispatch(userLogin({email,password,role}));
     }catch(error){
         console.log(error);
     }
@@ -9,8 +14,12 @@ export const handleLogin=(e,email,password,role)=>{
 export const handleRegister=(e,name,role,email,password,organisationName,hospitalName,website,address,phone)=>{
     e.preventDefault();
     try{
-        if(!role || !email || !password || !(organisationName || hospitalName || name ) || !website || !address || !phone) return alert("All fields are required");
-        console.log("Login",e,name,role,email,password,organisationName,hospitalName,website,address,phone);
+       if(!role || !email || !password || !website || !address || !phone) return alert("All fields are required");
+       if(((role==='donar' || role==='admin') && !name))return alert("All fields are required");
+       if(role==='organisation' && !organisationName)return alert("All fields are required");
+       if(role==='hospital' && !hospitalName)return alert("All fields are required");
+       // console.log("Login",e,name,role,email,password,organisationName,hospitalName,website,address,phone);
+       store.dispatch(userRegister({name,role,email,password,organisationName,hospitalName,website,address,phone}));
     }catch(error){
         console.log(error);
     }
